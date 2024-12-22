3db14ef34b720a446600825f496498ae09a3ce0d
chirang56 <tamgaubijiranchi114@gmail.com>
2024-12-22 23:51:33 +0545

    add enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import Navbar from "@/scenes/navbar/";
 import { useState } from "react";
 
+enum SelectedPage {
+  Home = "home",
+  About = "about",
+  Services = "services",
+  Contact = "contact",
+}
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home');
+  const [selectedPage, setSelectedPage] = useState<string>(
+    SelectedPage.Home
+  );
 
   return (
       <div className="app bg-stone-200">
