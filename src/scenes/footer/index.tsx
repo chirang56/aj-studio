d47a3ae5e0a127df393abd0ca9042bfca9704961
chirang56 <tamgaubijiranchi114@gmail.com>
2025-02-02 16:40:25 +0545

    add copyright message in the footer section

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -5,7 +5,19 @@ const Footer = (props: Props) => {
   return (
   <footer className="bg-primary-100 py-16">
     <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
-    Footer
+        <div className="mt-16 basis-1/2 md:mt-0">
+            <p className="w-1 text-black font-serif font-bold"> CHIRANJIBI
+              <span className="bg-gradient-to-br from-violet-800 to-rose-600 
+              text-transparent bg-clip-text"> GAUTAM </span> 
+            </p>
+            <p className="my-5"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio 
+                dolorem neque optio, harum omnis aperiam minima non sunt mollitia corporis 
+                labore, commodi eius quo culpa molestiae! Deleniti atque eius qui!
+            </p>
+            <p> Copyright &copy; Chiranjibi Gautam; All Rights Reserved. </p>
+        </div>
+        <div></div>
+        <div></div>
     </div>
   </footer>
   );
